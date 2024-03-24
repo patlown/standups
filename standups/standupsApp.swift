08172433c7952f249746807d2f721716f7d80305
Pat Lown <patlown@pm.me>
2024-03-24 17:20:46 -0600

    Initial Commit

---
+++ standups/standupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  standupsApp.swift
+//  standups
+//
+//  Created by Patrick Lown on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct standupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
