f1b6cf28c6b81e27eb6ac5b07a36f1ca4683e81f
Pat Lown <patlown@pm.me>
2024-03-27 20:01:56 -0600

    feat: integrate standup list and form

--- standups/StandupsList.swift
+++ standups/StandupsList.swift
@@ -12,28 +12,51 @@ import ComposableArchitecture
 struct StandupsListFeature {
     @ObservableState
     struct State {
+        @Presents var addStandup: StandupFormFeature.State?
         var standups: IdentifiedArrayOf<Standup> = []
     }
     
     enum Action {
         case addButtonTapped
+        case addStandup(PresentationAction<StandupFormFeature.Action>)
+        
+        case cancelStandupButtonTapped
+        case saveStandupButtonTapped
     }
     
+    
+    @Dependency(\.uuid) var uuid
     var body: some ReducerOf<Self> {
         Reduce { state, action in 
             switch action {
+                
+            case .cancelStandupButtonTapped:
+                state.addStandup = nil
+                return .none
+                
+            case .saveStandupButtonTapped:
+                guard let standup = state.addStandup?.standup
+                else { return .none }
+                state.standups.append(standup)
+                state.addStandup = nil
+                return .none
+                
+            case .addStandup:
+                return .none
+                
             case .addButtonTapped:
-                state.standups.append(
-                    Standup(id: UUID(), theme: .allCases.randomElement()!)
-                )
+                state.addStandup = StandupFormFeature.State(standup: Standup(id: uuid()))
                 return .none
             }
+        }
+        .ifLet(\.$addStandup, action: /Action.addStandup) {
+            StandupFormFeature()
         }
     }
 }
 
 struct StandupsList: View {
-    let store: StoreOf<StandupsListFeature>
+    @Bindable var store: StoreOf<StandupsListFeature>
     
     var body: some View {
         List {
@@ -48,6 +71,24 @@ struct StandupsList: View {
                 Button("Add") {
                     store.send(.addButtonTapped)
                 }
+            }
+        }
+        .sheet(item: $store.scope(state: \.addStandup, action: \.addStandup)) { standupFormFeature in
+            NavigationStack {
+                StandupFormView(store: standupFormFeature)
+                    .navigationTitle("New standup")
+                    .toolbar {
+                        ToolbarItem {
+                            Button("Save") {
+                                store.send(.saveStandupButtonTapped)
+                            }
+                        }
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                store.send(.cancelStandupButtonTapped)
+                            }
+                        }
+                    }
             }
         }
     }
